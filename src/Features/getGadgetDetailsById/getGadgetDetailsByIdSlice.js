a0baa7964c48b7d33a5ccc2704c044bc1d731e84
hossainshofiq <ah731710@gmail.com>
2025-04-20 17:34:56 +0600

    update and fix fetchGadgetDetails function and minor change in slice in getGadgetDetailsByIdSlice

--- src/Features/getGadgetDetailsById/getGadgetDetailsByIdSlice.js
+++ src/Features/getGadgetDetailsById/getGadgetDetailsByIdSlice.js
@@ -1,42 +1,56 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { BASE_URL } from "../../SharedUtilities/SharedUtilities.jsx";
+import useAxiosPublic from "../../CustomHooks/useAxiosPublic.jsx";
+
 
 const initialState = {
   gadgetDetails: null,
   isLoading: false,
   isError: false,
   error: null,
-};
+}
+
 
 export const fetchGadgetDetails = createAsyncThunk(
   "getGadgetDetailsById/fetchGadgetDetails",
-  async (id) => {
-    const response = await fetch(
-      `${BASE_URL}/gadgets/get_gadget_details_by_id/${id}`,
-      {}
-    );
-    return await response.json();
+  async (id, { rejectWithValue }) => {
+    try {
+      const axiosPublic = useAxiosPublic();
+      const response = await axiosPublic.get(`/gadgets/get_gadget_details_by_id/${id}`, {});
+      if (response?.data?.status !== 200)
+        throw new Error(response?.data?.message || "Failed to fetch gadget details");
+      return response?.data?.data;
+    }
+    catch (error) {
+      return rejectWithValue(error.message);
+    }
   }
 );
 
+
 const getGadgetDetailsByIdSlice = createSlice({
-  name: "getGadgetDetailsById",
+  name: 'getGadgetDetailsById',
   initialState,
   extraReducers: (builder) => {
     builder.addCase(fetchGadgetDetails.pending, (state) => {
-      state.isError = false;
+      state.gadgetDetails = null;
       state.isLoading = true;
+      state.isError = false;
+      state.error = null;
     });
     builder.addCase(fetchGadgetDetails.fulfilled, (state, action) => {
       state.gadgetDetails = action.payload;
       state.isLoading = false;
+      state.isError = false;
+      state.error = null;
     });
     builder.addCase(fetchGadgetDetails.rejected, (state, action) => {
-      state.isError = true;
-      state.error = action.error?.message;
+      state.gadgetDetails = null;
       state.isLoading = false;
-    });
-  },
-});
+      state.isError = true;
+      state.error = action.payload;
+    })
+  }
+})
+
 
 export default getGadgetDetailsByIdSlice.reducer;

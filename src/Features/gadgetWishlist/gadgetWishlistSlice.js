90f22611c8359d7f78b8def8daacad445d2147f5
hossainshofiq <ah731710@gmail.com>
2025-04-20 17:25:46 +0600

    update and fix getWishlistGadgetsDetails and addOrRemoveWishlistGadget function in gadgetWishListSlice

--- src/Features/gadgetWishlist/gadgetWishlistSlice.js
+++ src/Features/gadgetWishlist/gadgetWishlistSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { BASE_URL } from "../../SharedUtilities/SharedUtilities.jsx";
+import { getUserProfileDetails } from "../userProfileDetails/userProfileDetailsSlice.js";
+
 
 const initialState = {
   wishlistGadgetDetails: [],
@@ -9,62 +10,58 @@ const initialState = {
   error: null,
 };
 
+
 export const getWishlistGadgetsDetails = createAsyncThunk(
   "gadgetWishlist/getWishlistGadgetsDetails",
-  async (userEmail, { rejectWithValue }) => {
+  async ({ userEmail, axiosSecure }, { rejectWithValue }) => {
     try {
-      const response = await fetch(
-        `${BASE_URL}/gadgets/get_gadget_details_of_a_wishlist_array`,
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ userEmail }),
-        }
+      const response = await axiosSecure.post(
+        `/gadgets/get_gadget_details_of_a_wishlist_array`,
+        { userEmail }
       );
-      const data = await response.json();
-
-      if (data.status !== 200)
-        throw new Error(
-          data.message || "Failed to get wishlist gadget details!"
-        );
+      const data = await response.data;
+      if (data.status !== 200) throw new Error(data.message || "Failed to get wishlist gadgets details!");
       return data.data;
-    } catch (error) {
+    }
+    catch (error) {
       return rejectWithValue(error.message);
     }
   }
 );
+
 
 export const addOrRemoveWishlistGadget = createAsyncThunk(
   "gadgetWishlist/addOrRemoveWishlistGadget",
-  async ({ userEmail, gadgetId }, { rejectWithValue, dispatch }) => {
+  async ({ userEmail, gadgetId, axiosSecure }, { rejectWithValue, dispatch }) => {
     try {
-      const response = await fetch(
-        `${BASE_URL}/users/add_or_remove_a_gadget_id_to_or_from_wishlist`,
-        {
-          method: "PATCH",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ userEmail, gadgetId }),
-        }
+      const response = await axiosSecure.patch(
+        `/users/add_or_remove_a_gadget_id_to_or_from_wishlist`,
+        { userEmail, gadgetId }
       );
-      const data = await response.json();
-
-      if (data.status !== 200)
-        throw new Error(data.message || "Failed to update wishlist");
+      const data = await response.data;
+      if (data.status !== 200) throw new Error(data.message || "Failed to add or remove gadget to wishlist!");
 
       // Dispatch getWishlistGadgetsDetails after success
-      await dispatch(getWishlistGadgetsDetails({ userEmail }));
+      await Promise.all([
+        await dispatch(getWishlistGadgetsDetails({ userEmail, axiosSecure })),
+        await dispatch(getUserProfileDetails({ userEmail, axiosSecure }))
+      ])
 
       return data.data;
-    } catch (error) {
+    }
+    catch (error) {
       return rejectWithValue(error.message);
     }
   }
 );
+
 
 const gadgetWishlistSlice = createSlice({
   name: "gadgetWishlist",
   initialState,
   extraReducers: (builder) => {
+
+
     //To get the all gadget details from a user's wishlist array.
     builder.addCase(getWishlistGadgetsDetails.pending, (state) => {
       state.isLoading = true;
@@ -82,6 +79,7 @@ const gadgetWishlistSlice = createSlice({
       state.isError = true;
       state.error = action.payload; // Use rejectWithValue message
     });
+
 
     //To add or remove a gadget id to or from a user's wishlist array.
     builder.addCase(addOrRemoveWishlistGadget.pending, (state) => {
@@ -102,5 +100,6 @@ const gadgetWishlistSlice = createSlice({
     });
   },
 });
+
 
 export default gadgetWishlistSlice.reducer;
